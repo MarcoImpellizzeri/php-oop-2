0d571760c91b8f3828e5a6e007fbd459cd1f4e96
MarcoImpellizzeri <impellizzeri.marco18@gmail.com>
2023-09-20 14:22:07 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+require_once __DIR__ . '/calsses/Prodotto.php';
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="imgs/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="imgs/favicon.ico" type="image/x-icon">
+
+    <title>php-oop-2</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <link rel="stylesheet" href="/style/style.css">
+</head>
+
+<body>
+    <div class="container py-5">
+        <div class="row row-cols-2">
+            <div class="col">
+                <div class="card">
+                    <img src="" class="card-img-top" alt="image not found">
+                    <div class="card-body">
+                        <h2 class="card-title"></h2>
+                        <p class="card-text">
+                            
+                        </p>
+                        <p class="card-text"></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
